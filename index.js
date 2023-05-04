7761075f75187ad57305fb10c82217b15a63bd5b
Mahin01 <Ashmahin4@gmail.com>
2023-05-04 15:21:28 +0600

    CORS installed to avoid error when fetching data from client side

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+app.use(cors());
 
 const chefData = require('./data/chefsData.json');
 const chefRecipe = require('./data/chefsRecipe.json');
