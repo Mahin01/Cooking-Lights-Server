5c126b7668b019084ba1c641983aa57cceb0a298
Mahin01 <Ashmahin4@gmail.com>
2023-05-04 15:13:14 +0600

    Data Loaded On the backend Server

--- index.js
+++ index.js
@@ -2,10 +2,21 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const chefData = require('./data/chefsData.json');
+const chefRecipe = require('./data/chefsRecipe.json');
+
 app.get('/', (req, res) => {
     res.send('Cooking Lights Running');
-  })
+});
+
+app.get('/chefs-data', (req, res) => {
+    res.send(chefData);
+});
+
+app.get('/chefs-recipe', (req, res) => {
+    res.send(chefRecipe);
+});
   
-  app.listen(port, () => {
-    console.log(`Cooking Lights running on port: ${port}`)
-  })
+app.listen(port, () => {
+  console.log(`Cooking Lights running on port: ${port}`)
+});
